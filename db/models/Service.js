f98fdf81a0b23be9a691a3dadc998e806c07c255
amnah5 <amooon_96@hotmail.com>
2021-12-06 20:21:21 +0300

    detail

--- db/models/Service.js
+++ db/models/Service.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const ServiceSchema = new mongoose.Schema({
   name: { type: String },
   image: { type: String },
+  description: {type : String}
 });
 
 module.exports = model("Service", ServiceSchema);
