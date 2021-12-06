6effefaddcf77ee592c6452fb99850c26cd93ba7
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-12-06 17:06:29 +0300

    images-render

--- App.js
+++ App.js
@@ -29,9 +29,9 @@ passport.use(jwtStrategy);
 //Routes
 app.use("/api", userRoutes);
 app.use("/api", serviceRoutes);
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Path not found" });
-});
+
+app.use("/media", express.static(path.join(__dirname, "media")));
+
 
 app.use(errorHandler);
 
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: "./media/pic_name",
+  destination: "./media",
   filename: (req, file, cb) => {
     cb(null, `${+new Date()}${file.originalname}`);
   },
